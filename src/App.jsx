d06f31b4b08fd251d60ab05f0099ee6fe861eb8e
aneeketmoheete23-ui <aneeketmoheete23@gmail.com>
2026-09-18 03:23:41 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>LeakLeans</h1>
+      <p>Find Where Your Revenue Is Leaking.</p>
+
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;

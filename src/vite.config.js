88ad610015d45a287802fc4ff30aad00f309473c
aneeketmoheete23-ui <aneeketmoheete23@gmail.com>
2026-09-18 16:34:26 +0530

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 import vercel from "vite-plugin-vercel";
 
 export default defineConfig({
+  server: {
+    port: process.env.PORT,
+  },
   plugins: [react(), vercel()],
 });
